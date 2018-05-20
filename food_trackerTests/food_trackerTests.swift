2e53634264856d2211a44a9cc001686177aab293
Emily Chatterjee <echatter@umich.edu>
2018-05-20 10:09:45 -0400

    Initial Commit

---
+++ food_trackerTests/food_trackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  food_trackerTests.swift
+//  food_trackerTests
+//
+//  Created by Emily Denise Chatterjee on 5/20/18.
+//  Copyright © 2018 Emily Denise Chatterjee. All rights reserved.
+//
+
+import XCTest
+@testable import food_tracker
+
+class food_trackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

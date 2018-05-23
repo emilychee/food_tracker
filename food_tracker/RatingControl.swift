0054238516215c7da22560aecbd7ef5f65f59b56
Emily Chatterjee <echatter@umich.edu>
2018-05-23 15:31:23 -0400

    added rating control file

---
+++ food_tracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  food_tracker
+//
+//  Created by Emily Denise Chatterjee on 5/23/18.
+//  Copyright © 2018 Emily Denise Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}

0eebc6733de433ef1ec7e3440d3cd4f80f957fe7
Emily Chatterjee <echatter@umich.edu>
2018-06-05 15:25:25 -0400

    created Meal.swift file

---
+++ food_tracker/Meal.swift
@@ -0,0 +1,17 @@
+//
+//  Meal.swift
+//  food_tracker
+//
+//  Created by Emily Denise Chatterjee on 6/5/18.
+//  Copyright © 2018 Emily Denise Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+}

33fc7ecefcf44fb1967a66e40a79cefe28bf9106
Emily Chatterjee <echatter@umich.edu>
2018-06-05 15:50:50 -0400

    made a Meal class

--- food_tracker/Meal.swift
+++ food_tracker/Meal.swift
@@ -14,4 +14,17 @@ class Meal {
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialization should fail if there's no name or the rating is negative
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
 }
